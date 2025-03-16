cc77c3a255af584418e97e03933ababec8f514b3
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2025-03-16 07:16:21 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -783,6 +783,9 @@ def index():
     """Welcome endpoint."""
     return "Welcome to the Face Recognition System API"
 
+#if __name__ == '__main__':
+ #   port = int(os.getenv('PORT', 5000))
+  #  app.run(host='0.0.0.0', port=port, debug=True)
+
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
